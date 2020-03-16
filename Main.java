31c40487e341ade378a7bd85d5e585ca23b649fc
melgage <54865040+melgage@users.noreply.github.com>
2020-03-16 10:29:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package com.smartbear.demo;
 import java.util.Scanner;
 
-// Change One Rev 1
+// Change One
 
 public class Main {
 
@@ -12,7 +12,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
+        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
     }
 
     // Java program for Fibonacci number using Loop.
