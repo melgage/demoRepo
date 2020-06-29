c525a6ecec376ede2a35baddb4bac777f1fef64d
Mel Gage <melgage@gmail.com>
2020-06-29 11:17:53 -0400

    Initial commit

--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 
 // Java development file
 // Initial commit
-// Change one
-// Change Two.Rev
-
 
 public class Main {
 
@@ -15,7 +12,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
+        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
     }
 
     // Java program for Fibonacci number using Loop.
