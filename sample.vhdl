4361d0c2d1ed0d9026adc17bef21c0f568e575e7
Mel Gage <mel.gage@smartbear.com>
2020-11-09 15:48:19 -0500

    Change One

--- sample.vhdl
+++ sample.vhdl
@@ -1,6 +1,10 @@
 -------------------------------------------------------------------------------
 -- File Downloaded from http://www.nandland.com
 -------------------------------------------------------------------------------
+
+-- Change One
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
