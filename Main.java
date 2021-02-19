869b0b940d4bfd59b1ebd61225e15ad88ac66d24
Mel Gage <mel.gage@smartbear.com>
2021-02-19 08:27:41 -0500

    Clean Files

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 // Java development file
 // Initial commit
 // 
-// Change One
-// Change Two
-// Change Three
-// Change Four
-// Change Five
+// 
+// 
+// 
+// 
+
 
 public class Main {
 
@@ -17,7 +17,7 @@ public class Main {
         if(number == 1 || number == 2){
             return 1;
         }
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
+        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
     }
 
     // Java program for Fibonacci number using Loop.
@@ -25,9 +25,7 @@ public class Main {
         if(number == 1 || number == 2){
             return 1;
         }
-        long fibo1=1,
-             fibo2=1,
-             fibonacci=1;
+        long fibo1=1,fibo2=1,fibonacci=1;
         for(int i= 3; i<= number; i++){
             fibonacci = fibo1 + fibo2; //Fibonacci number is sum of previous two Fibonacci number
             fibo1 = fibo2;
