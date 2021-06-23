d9dd70cf0341bb6f519b949cd3d36d2afbb22a7b
Mel Gage <mel.gage@smartbear.com>
2021-06-23 13:14:23 -0400

    Change One Rev 1

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //
 //
 //
-// Change One
+// Change One Rev 1
 //
 // 
 
@@ -18,7 +18,7 @@ public class Main {
         if(number == 1 || number == 2){
             return 1;
         }
-        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
+        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
     }
     // Java program for Fibonacci number using Loop.
     public static long fibonacciLoop(int number){
