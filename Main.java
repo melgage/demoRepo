a346c3a0b53063cadbe1a9d69f59ae9d822fb78c
Mel Gage <mel.gage@smartbear.com>
2021-10-28 10:50:55 -0400

    Clean Files

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ package com.smartbear.demo;
 import java.util.Scanner;
 
 // Java development file
-// Initial commit
+//	 Initial commit
 //
-// Change One. Rev 1
+//
 //
 //
 //
@@ -18,7 +18,7 @@ public class Main {
         if(number == 1 || number == 2){
             return 1;
         }
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
+        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
     }
     // Java program for Fibonacci number using Loop.
     public static long fibonacciLoop(int number){
