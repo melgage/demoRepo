7cc22a4b24a68088ed7cb5a0a9ee1ca57c09c45b
melgage <mel.gage@smartbear.com>
2022-05-24 11:17:01 -0400

    Clean up

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 // Java development file
 //	 Initial commit
 //
-// Change One Rev 1
 //
 //
 public class Main {
@@ -14,7 +13,7 @@ public class Main {
         if(number == 1 || number == 2){
             return 1;
         }
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
+        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //tail recursion
     }
     //
 //
